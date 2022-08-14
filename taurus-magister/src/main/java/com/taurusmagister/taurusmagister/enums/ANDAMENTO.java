662e8c9961724c65742016468e4d17a8ccd7d598
RobertoAlve <roberto.alves@bandtec.com.br>
2022-08-14 18:39:08 -0300

    Atualizando Api

--- taurus-magister/src/main/java/com/taurusmagister/taurusmagister/enums/ANDAMENTO.java
+++
@@ -1,9 +0,0 @@
-package com.taurusmagister.taurusmagister.enums;
-
-public enum ANDAMENTO {
-    ;
-    public static final String EM_ANDAMENTO = "Em andamento" ;
-    public static final String FINALIZADA = "Finalizada";
-    public static final String DEFAULT = "Não iniciada";
-
-}
--- taurus-magister/src/main/java/com/taurusmagister/taurusmagister/repositorio/TransacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taurusmagister.taurusmagister.repositorio;
-
-import com.taurusmagister.taurusmagister.entidade.Transacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransacaoRepository extends JpaRepository<Transacao, Integer> {
-}
