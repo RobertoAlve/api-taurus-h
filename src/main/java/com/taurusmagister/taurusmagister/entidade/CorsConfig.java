67680737681f662a76cd4954ff1f741f081d5ff8
RobertoAlve <roberto.alves@bandtec.com.br>
2022-09-07 15:24:32 -0300

    Atualizando Api

--- src/main/java/com/taurusmagister/taurusmagister/entidade/CorsConfig.java
+++ src/main/java/com/taurusmagister/taurusmagister/entidade/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://sistema-taurus.herokuapp.com", "http://localhost:3000/", "http://54.173.177.100:3000/")
+                .allowedOrigins("https://sistema-taurus.herokuapp.com", "http://localhost:3000/", "http://54.173.177.100:3000/",
+                        "http://taurus.zapto.org", "https://taurus.zapto.org")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT", "PATCH");
     }
 }
